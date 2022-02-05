31fd7fc0a82d918d3a282453e79fd6d868ce58f8
Mahmoud Fawzy <43361910+MahmoudFawzy01@users.noreply.github.com>
2022-02-05 02:52:11 +0200

    Signed-off-by: Mahmoud Fawzy <43361910+MahmoudFawzy01@users.noreply.github.com>

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', '.hbs');
 
 // Add single reference to the model into application object
 // and reuse it whenever an access to DB is needed
-//app.set('db_model', require('./lib/model/db'));
+app.set('db_model', require('./lib/model/db'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -38,9 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Setup authentication mechanism
 const passport = require('./lib/passport')();
 
-/*app.use(createSessionMiddleware({
+app.use(createSessionMiddleware({
   sequelizeDb: app.get('db_model').sequelize,
-}))*/
+}))
 app.use(passport.initialize());
 app.use(passport.session());
 
